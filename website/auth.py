d5945be6687b90043da524b5d63337b7e2421c42
Pabllot07 <pabllot07@outlook.com>
2024-05-26 19:37:56 -0300

    feat: sign up taken care of

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint,render_template,request, flash
+from flask import Blueprint,render_template,request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -14,13 +17,19 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if len(email) < 4:
-            flash("email must be at least 4 characters", category="error")
+        if len(first_name) < 4:
+            flash("name must have at least 4 characters", category="error")
+        elif password1 != password2:
+            flash("passwords do not match", category="error")
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created', category='sucess')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user='Pablo')
