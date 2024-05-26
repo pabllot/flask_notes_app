790ff2cf4457cd09ebec6f7ff3c3074815033f2e
Pabllot07 <pabllot07@outlook.com>
2024-05-26 20:14:55 -0300

    feat: redirects based if user is logged in

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint,render_template,request, flash, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -15,18 +16,20 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash("Logged in successfully", category="success")
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash("Invalid password, try again", category="error")
         else:
             flash("Email not found", category="error")
 
-
-    return render_template("login.html",user='ds')
+    return render_template("login.html",user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -49,6 +52,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash('Account created', category='sucess')
+            login_user(user, remember=True)
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html", user='Pablo')
+    return render_template("sign_up.html", user=current_user)
