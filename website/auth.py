2deb14ed0c967c763e141aafe74cca5561b4125c
Pabllot07 <pabllot07@outlook.com>
2024-05-26 18:26:04 -0300

    feat: html templates

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint,render_template,request, flash
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html",user='ds')
+
+@auth.route('/logout')
+def logout():
+    return "<p>logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash("email must be at least 4 characters", category="error")
+        else:
+            flash('Account created', category='sucess')
+
+    return render_template("sign_up.html", user='Pablo')
