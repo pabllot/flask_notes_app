326804feb2ff7b2f14466e1baed4ca279ef3acdd
Pabllot07 <pabllot07@outlook.com>
2024-05-26 17:33:51 -0300

    feat: blueprints configured

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'lçsSAJFD87kjfs9s8'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/')
+
     return app
